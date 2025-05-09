4fe804b0fd224040266393c0388586ba33bc0705
Jacob Meyer <jacobmeyer221@gmail.com>
2025-05-09 16:08:17 -0500

    make description container/text the same

--- components/TimelineModalDescription.tsx
+++ components/TimelineModalDescription.tsx
@@ -4,7 +4,7 @@ import type { TimelineModalDescriptionProps } from "./TimelineModal.types";
 const TimelineModalDescription: React.FC<Readonly<TimelineModalDescriptionProps>> = ({ description, descriptionRef }) => (
   <div
     ref={descriptionRef}
-    className="overflow-y-auto text-base md:text-lg text-muted-foreground mb-2 text-center w-full max-w-2xl break-words rounded bg-muted/70 px-3 py-2"
+    className="overflow-y-auto text-base md:text-lg text-muted-foreground mb-2 text-center w-full break-words bg-muted/70 py-2"
     style={{ height: '220px' }}
     tabIndex={0}
   >
